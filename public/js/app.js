9a6466afde623e0dc6ca76e9759fe1c8becb2de5
Arkadi <mrarkadi@gmail.com>
2017-07-05 20:57:54 +0300

    added bunch of files and code to setup ... sending emails via contact form

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,16 @@
 var app = angular.module('emunApp', ['ui.router']);
+
+app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
+  $locationProvider.html5Mode(true);
+  $urlRouterProvider.otherwise('/home');
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'index.html'
+    })
+    .state('contact', {
+      url: '/contact',
+      templateUrl: '/templates/contact.html',
+      controller: 'mailController'
+    })
+});
