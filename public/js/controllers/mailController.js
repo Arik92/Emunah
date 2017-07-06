a43e58e30b8fe68f1101e5f425d383726dd8f36a
Arkadi <mrarkadi@gmail.com>
2017-07-06 20:02:55 +0300

    added packages for contact via email functionality

--- public/js/controllers/mailController.js
+++ public/js/controllers/mailController.js
@@ -4,12 +4,12 @@ app.controller('mailController', function ($scope, $http) {
     $scope.loading = true;
     $http.post('/sendmail', {
       from: 'It is Arik <mbreslow56@gmail.com>',
-      to: 'mbreslow56@gmail.com',
+      to: 'mrarkadi@gmail.com',
       subject: 'Message from maynard?',
       text: mail.message
     }).then(res => {
         $scope.loading = false;
         $scope.serverMessage = 'Your email was sent successfully. Thanks for contacting us!';
-    });
+    }).catch(err => console.log(err));
   }
 })
