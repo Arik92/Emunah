a6152aa87eb3152e8e9b03cecc68e0f57caad154
Arik92 <arik92@gmail.com>
2017-08-16 20:06:04 +0300

    youtube playlists can now go forward and fetch more

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -21,13 +21,73 @@ $scope.getAllPlayLists = function() {
       console.error("controller error fetching playlists");
     } else {
       console.log("playlist data is", data);
-      $scope.currentPlaylists = data.items;
+      $scope.allPlayLists = data.items; // an array of 48 playLists
+      $scope.playListIndex = 0;
+      $scope.numRes = data.pageInfo.totalResults;
+      $scope.currentPlaylists = [];
+      $scope.updatePlaylistForward();
       $scope.nextToken = data.nextPageToken;
-
-      //$scope.nextToken =
     }//else
   });//callback
 }//getAllPlaylists
+
+$scope.getNextPlayLists = function() {
+  ytService.getNextPlayLists($scope.nextToken).then(function(data, err){
+    if (err) {
+      console.log("controller error fetching next playlists");
+    } else {
+      for (var i = 0;i<data.items.length;i++) {
+          $scope.allPlayLists.push(data.items[i]);
+      }//for
+      console.log("array after fetch loop", $scope.allPlayLists);
+      $scope.prevToken = data.prevPageToken;
+    }//else
+  })
+}//fetch next playlists
+
+$scope.getPrevPlayLists = function() {
+  ytService.getNextPlayLists($scope.prevToken).then(function(data, err){
+    if (err) {
+      console.log("controller error fetching next playlists");
+    } else {
+      $scope.currentPlaylists = data.items;
+      $scope.nextToken = data.nextPageToken;
+    }//else
+  })
+}//fetch prev playlists
+
+$scope.updatePlaylistForward = function() {
+  var max;
+  var diff = $scope.allPlayLists.length-$scope.playListIndex;
+  if ((diff<=5)&&($scope.playListIndex+diff<$scope.numRes)) {
+    $scope.getNextPlayLists();
+      var count = 0;
+    $scope.currentPlaylists = [];
+    for (var i = $scope.playListIndex;i<$scope.playListIndex+6;i++) {
+      // $scope.currentPlaylists.push($scope.allPlayLists[i]); //NOTE: We might actually want this behaviour
+      $scope.currentPlaylists[count] = $scope.allPlayLists[i];
+      count++;
+    }//for update playlist
+    $scope.playListIndex+=6;
+    console.log("playlist index is now", $scope.playListIndex);
+  } else {
+    if (diff<=5) {
+      max = diff;  //if we reached here, these are the last 5 or less results
+    } else {
+      max = 6;
+    }
+      var count = 0;
+      $scope.currentPlaylists = [];
+      for (var i = $scope.playListIndex;i<$scope.playListIndex+max;i++) {
+        // $scope.currentPlaylists.push($scope.allPlayLists[i]); //NOTE: We might actually want this behaviour
+        $scope.currentPlaylists[count] = $scope.allPlayLists[i];
+        count++;
+      }//for update playlist
+      $scope.playListIndex+=max;
+      console.log("playlist index is now", $scope.playListIndex);
+    }//else if update shouldnt bring the rest of the results
+}//UPF
+
 
 ////////////*************************PLAYLIST INTERFACE **********************************8////////////////////////
 //invokes //
--- public/js/services/youtubeService.js
+++ public/js/services/youtubeService.js
@@ -17,14 +17,21 @@ app.service('ytService', function($http) {
       console.error("error during api request", error);
     });
   }//getAllPlaylists
+  serv.getNextPlayLists = function(Token) {
+    return $http.get('https://www.googleapis.com/youtube/v3/playlists?pageToken='+Token+'&part=snippet&maxResults=48&channelId=UCtAh700VTIQb5Wsx_vdg-Pw&key=' + config.YOUTUBE_KEY).then(function(response){
+      console.log("next playlists are:", response.data);
+      return response.data;
+    }, function(error){
+      console.error("error fetching the next playlists");
+     });
+  }
   serv.getLatestVids = function(num) {
-    return $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCtAh700VTIQb5Wsx_vdg-Pw&maxResults='+num+'&order=date&type=video&key=' + config.YOUTUBE_KEY).then(function(response) {
+    return $http.get('https://www.googleapis.com/youtube/v3/search?&part=snippet&channelId=UCtAh700VTIQb5Wsx_vdg-Pw&maxResults='+num+'&order=date&type=video&key=' + config.YOUTUBE_KEY).then(function(response) {
       //console.log('latest response', response);
       return response.data;
     }, function(error) {
       console.error("error fetching latest vids");
     });
-
   }//getLatesVids
   return serv;
 })//service
