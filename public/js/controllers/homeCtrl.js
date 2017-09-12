11a6da6ce079a5d461060ce9765de59806ea85fc
Arik92 <arik92@gmail.com>
2017-09-12 17:44:39 +0300

    I have something. WIP

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -1,13 +1,17 @@
 app.controller('homeCtrl', function($rootScope, $scope, authFactory, ytService) {
   console.log("auth user data to work with", authFactory.currentUser);
   $rootScope.currentUser = authFactory.currentUser.email;
+  $scope.topThree = [];
   ////////****************************LATEST VIDEO INTERFACE ******************************8/////////////////////
   $scope.getLatestVids = function(num) {
   ytService.getLatestVids(num).then(function(data, err){
     if (err) {
       console.error("controller error fetching latest videos", err);
     } else {
+      $scope.topThree = data.items.splice(0,3);
+      console.log("top three videos are ", $scope.topThree);
       $scope.latestVids = data.items;
+
       console.log("latest vids are", data.items);
     }//else
   })//callback
@@ -123,6 +127,6 @@ $scope.setCurr = function(obj, type) {
 
 ////////////*************************PLAYLIST INTERFACE **********************************8////////////////////////
 //invokes //
-$scope.getLatestVids(3);
+$scope.getLatestVids(23);
 $scope.getAllPlayLists();
 }); //controller
