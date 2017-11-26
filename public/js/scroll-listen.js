938baa346b7d944dade44363f086a18b4b6497db
Natti Katz <katzy687@gmail.com>
2017-11-26 22:56:57 +0200

    fixed store nav, still need to optimize scroll events

---
+++ public/js/scroll-listen.js
@@ -0,0 +1,13 @@
+// document.addEventListener("scroll", transform);
+
+// function transform(e) {
+//   console.log('fired transform');
+//   const storeMenu = document.querySelector('.store-menu-overlay');
+//   const offsetCheck = window.pageYOffset > 78;
+//   if (offsetCheck && storeMenu) {
+//     storeMenu.classList.add('transform');
+//     // document.removeEventListener("scroll", transform);
+//   } else {
+//     storeMenu.classList.remove('transform');
+//   }
+// }
