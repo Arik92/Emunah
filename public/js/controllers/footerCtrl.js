556bcdd09ae02f71a2e860b96454137a42309e32
Natti Katz <katzy687@gmail.com>
2017-12-05 20:27:09 +0200

    finished footer

--- public/js/controllers/footerCtrl.js
+++ public/js/controllers/footerCtrl.js
@@ -1,10 +1,12 @@
 app.controller('footerCtrl', function($scope, linkService) {
-  $scope.footerLinks = [];
+  $scope.footerLinkColumns = [];
+  $scope.socialMedia = [];
 
   this.$onInit = () => {
     console.log('yo from footerCtrl');
-    $scope.footerLinks = linkService.footerLinks;
-    console.log($scope.footerLinks);
+    $scope.footerLinkColumns = linkService.footerLinkColumns;
+    $scope.socialMedia = linkService.socialMedia;
+    console.log($scope.footerLinkColumns);
   }
 });//singleCtrl
 
