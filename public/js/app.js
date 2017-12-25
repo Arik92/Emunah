a008a1e1e226d45926ed1477ece4cb7d1b9508ae
mbreslow56 <mbreslow56@gmail.com>
2017-12-25 03:08:40 +0200

    added new free israel form and page for israel, fixed nav stuff

--- public/js/app.js
+++ public/js/app.js
@@ -153,8 +153,8 @@ app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
       url: '/articles',
       templateUrl: '/templates/articles.html'
     })
-	 .state('free', {
-      url: '/free-trip-to-israel',
+	 .state('free1', {
+      url: '/free-trip-to-israel1',
       templateUrl: '/templates/freeisrael.html',
       controller: 'freeCtrl'
     })
@@ -287,6 +287,14 @@ app.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
     .state('about', {
       url: '/about',
       templateUrl: '/templates/about/about.html',
+    })
+    .state('free', {
+      url: '/free-trip-to-israel',
+      templateUrl: '/templates/temp/freeisrael.html',
+    })
+    .state('visitisrael', {
+      url: '/visit-israel-rav-dror',
+      templateUrl: '/templates/temp/israeltrip.html',
     })
     .state('chayei-sarah', {
       url: '/becoming-closer-to-hashem-good-eyes-a-pure-heart',
